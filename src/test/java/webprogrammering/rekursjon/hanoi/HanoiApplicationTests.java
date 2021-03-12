fdbae1310cbcc5527b1b1200507f9b02e7731eba
JorBernt <berntsen.jorgen@gmail.com>
2021-03-12 09:01:48 +0100

    Initial commit

---
+++ src/test/java/webprogrammering/rekursjon/hanoi/HanoiApplicationTests.java
@@ -0,0 +1,13 @@
+package webprogrammering.rekursjon.hanoi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HanoiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
